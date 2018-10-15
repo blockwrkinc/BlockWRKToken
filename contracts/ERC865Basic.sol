961122afeca6036671838f002d4b5da817b0e8d9
Jef Davis <j.davis@intellimatix.co>
2018-10-15 15:08:38 -0400

    refactor ERC865Basic.sol

--- contracts/ERC865Basic.sol
+++ contracts/ERC865Basic.sol
@@ -6,9 +6,8 @@ import '../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol
  * @title ERC865Basic
  * @dev Simpler version of the ERC865 interface from https://github.com/adilharis2001/ERC865Demo
  * @author jsdavis28
- * @notice ERC865Token allows for users to pay gas costs in an ERC20 token to a delegate
+ * @notice ERC865Token allows for users to pay gas costs to a delegate in an ERC20 token
  * https://github.com/ethereum/EIPs/issues/865
- *
  */
  contract ERC865Basic is ERC20 {
      function _transferPreSigned(
